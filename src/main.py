96690eb10c4f071561314489712708a53a5d93f8
OTOfngn <c98423795274152963@outlook.com>
2025-04-09 13:04:05 +0300

    Imported animations to main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from sense_hat import SenseHat
 from src.action import Action
+from features.animations import SenseHATAnimations
 # from signal import pause
 
 sense = SenseHat()
 action = Action(sense)
 temperature = sense.get_temperature() - 13
 pressure = int(sense.get_pressure())
+animation = SenseHATAnimations(sense = sense)
 
 # Initialization SenseHAT pressure sensor
 sense.stick.direction_up = action.pushed_up
