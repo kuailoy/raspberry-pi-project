bb21fb2c03d759b855ffb680bb3bf75052ee7aa7
kuailoy <kuailoy@foxmail.com>
2025-04-09 10:47:41 +0300

    update

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 from signal import pause
-import subprocess
+from datetime import datetime
 
+def get_system_time():
+    return datetime.now().strftime('%H:%M')
 
 #CONFIG
 Scroll = float(0.05)                                                            # Text Scroll speed
-O = [100, 100, 100]                                                             # WHITE
-X = [0, 0, 0]                                                                   # BLACK
+X = [100, 100, 100]                                                             # WHITE
+O = [0, 0, 0]                                                                   # BLACK
 R = [100, 0, 0]                                                                 # RED
 B = [0, 0, 100]                                                                 # BlUE
 G = [0, 100, 0]                                                                 # GREEN
 x = 3                                                                           # JOY-STICK X-Axis
 y = 3                                                                           # JOY-STICK Y-Axis
 sense = SenseHat()                                                              # Initialization SenseHAT
-temp = sense.get_temperature() - 13                                             # Initialization SenseHAT temperature sensor 
+temp = sense.get_temperature() - 13                                             # Initialization SenseHAT temperature sensor
 pressure = int(sense.get_pressure())                                            # Initialization SenseHAT pressure sensor
-result = subprocess.run(['python', 'ai.py'], capture_output=True, text=True)    # Initialization subprecess for AI part
 
 
 
@@ -57,13 +58,15 @@ def pushed_left(event):
     global x
     if event.action != ACTION_RELEASED:
         x = clamp(x - 1)
+        sense.show_message(get_system_time(), text_colour = R, scroll_speed = Scroll)
+    main()
 
 
 def pushed_right(event):
     global x
     if event.action != ACTION_RELEASED:
         x = clamp(x + 1)
-        print("AI OUTPUT", result.stdout)
+
 
 
 
