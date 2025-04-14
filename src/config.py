479c80c10e23d0274bb046fd6a9381a7b63de765
kuailoy <kuailoy@foxmail.com>
2025-04-14 14:27:21 +0300

    add acceleration

--- src/config.py
+++ src/config.py
@@ -8,3 +8,18 @@ G = [0, 100, 0]                                                                 
 x = 3                                                                           # JOY-STICK X-Axis
 y = 3                                                                           # JOY-STICK Y-Axis
 
+
+# green = (0, 255, 0)
+# yellow = (255, 255, 0)
+# blue = (0, 0, 255)
+# red = (255, 0, 0)
+# white = (255,255,255)
+# nothing = (0,0,0)
+# pink = (255,105, 180)
+# G = green
+# Y = yellow
+# B = blue
+# R = red
+# W = white
+# O = nothing
+# P = pink
--- src/features/animations.py
+++ src/features/animations.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 from sense_hat import SenseHat
 
 s = SenseHat()
@@ -116,8 +116,8 @@ class SenseHATAnimations:
         ]
         return logo
 
-    def heart(self):
-        P = pink
+    def heart(self, color=pink):
+        P = color
         O = nothing
         logo = [
         O, O, O, O, O, O, O, O,
@@ -130,7 +130,7 @@ class SenseHATAnimations:
         O, O, O, O, O, O, O, O,
         ]
         return logo
-    
+
     def menu_temperature():
         logo = [
         O, O, O, R, R, O, O, O,
@@ -143,20 +143,20 @@ class SenseHATAnimations:
         O, R, R, R, O, O, O, O
         ]
         return logo
-    
+
     def menu_pressure():
         logo = [
         O, B, B, B, B, B, O, O,
         O, B, O, O, O, O, B, O,
         O, B, O, O, O, O, B, O,
         O, B, O, O, O, O, B, O,
-        O, B, B, B, B, B, O, O, 
+        O, B, B, B, B, B, O, O,
         O, B, O, O, O, O, O, O,
         O, B, O, O, O, O, O, O,
         O, B, O, B, B, O, O, O
         ]
         return logo
-    
+
 
     def question_mark(self):
         O = [0, 0, 0]                                                                   # BLACK
@@ -176,3 +176,63 @@ class SenseHATAnimations:
             O, O, O, B, B, O, O, O
         ]
         return logo
+
+    def rainbow(self):
+        pixels = [
+            [255, 0, 0], [255, 0, 0], [255, 87, 0], [255, 196, 0], [205, 255, 0], [95, 255, 0], [0, 255, 13], [0, 255, 122],
+            [255, 0, 0], [255, 96, 0], [255, 205, 0], [196, 255, 0], [87, 255, 0], [0, 255, 22], [0, 255, 131], [0, 255, 240],
+            [255, 105, 0], [255, 214, 0], [187, 255, 0], [78, 255, 0], [0, 255, 30], [0, 255, 140], [0, 255, 248], [0, 152, 255],
+            [255, 223, 0], [178, 255, 0], [70, 255, 0], [0, 255, 40], [0, 255, 148], [0, 253, 255], [0, 144, 255], [0, 34, 255],
+            [170, 255, 0], [61, 255, 0], [0, 255, 48], [0, 255, 157], [0, 243, 255], [0, 134, 255], [0, 26, 255], [83, 0, 255],
+            [52, 255, 0], [0, 255, 57], [0, 255, 166], [0, 235, 255], [0, 126, 255], [0, 17, 255], [92, 0, 255], [201, 0, 255],
+            [0, 255, 66], [0, 255, 174], [0, 226, 255], [0, 117, 255], [0, 8, 255], [100, 0, 255], [210, 0, 255], [255, 0, 192],
+            [0, 255, 183], [0, 217, 255], [0, 109, 255], [0, 0, 255], [110, 0, 255], [218, 0, 255], [255, 0, 183], [255, 0, 74]
+        ]
+
+        msleep = lambda x: time.sleep(x / 1000.0)
+
+
+        def next_colour(pix):
+            r = pix[0]
+            g = pix[1]
+            b = pix[2]
+
+            if (r == 255 and g < 255 and b == 0):
+                g += 1
+
+            if (g == 255 and r > 0 and b == 0):
+                r -= 1
+
+            if (g == 255 and b < 255 and r == 0):
+                b += 1
+
+            if (b == 255 and g > 0 and r == 0):
+                g -= 1
+
+            if (b == 255 and r < 255 and g == 0):
+                r += 1
+
+            if (r == 255 and b > 0 and g == 0):
+                b -= 1
+
+            pix[0] = r
+            pix[1] = g
+            pix[2] = b
+
+        while True:
+            for pix in pixels:
+                next_colour(pix)
+
+            self.sense.set_pixels(pixels)
+            msleep(2)
+
+    def loading(self):
+        for i in range(5):
+            self.sense.set_pixels(self.raspi_logo())
+            sleep(0.3)
+            self.sense.clear()
+            sleep(0.3)
+            self.sense.set_pixels(self.heart())
+            sleep(0.3)
+            self.sense.clear()
+            sleep(0.3)
